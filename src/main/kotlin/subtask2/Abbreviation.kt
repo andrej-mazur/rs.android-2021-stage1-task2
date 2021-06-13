b6eebfaf16eb32f11a601a37da0596e9f3c18ed8
Andrej Mazur <andrej.mazur@outlook.com>
2021-06-14 00:55:44 +0300

    feat: add second subtask

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,21 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var lastMatchIndexA = -1
+        var counter = 0
+        loopB@ for (indexB in b.indices) {
+            for (indexA in (lastMatchIndexA + 1) until a.length) {
+                if (a[indexA].equals(b[indexB], ignoreCase = true)) {
+                    lastMatchIndexA = indexA
+                    counter++
+                    continue@loopB
+                } else if (a[indexA].isUpperCase()) {
+                    break@loopB
+                }
+            }
+        }
+        return if (counter == b.length) "YES" else "NO"
     }
+
 }
